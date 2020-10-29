38e57f31a7eac12a78b98ccbcf9851b4b562cb64
AlanMondra <a01734565@itesm.mx>
2020-10-29 01:29:45 +0000

    Regresaré

--- Tarea2.py
+++ Tarea2.py
@@ -1,7 +1,20 @@
+# %%
+# PARTE 1
 import pandas as pd
-
-df = pd.read_csv('ulabox_orders_with_categories_partials_2017.csv')
-
+df = pd.read_csv('ulabox.csv')
+# %%
+# PARTE 2
+del df['order'] #eliminamos order ya que no nos es relevante
 print("Tamaño del dataset:",df.shape)
-df.info()
-# df.describe()
+print("\nTIPO DE VARIABLE:\nCustumer = continuo\nTotal = continuo\nDiscount = discreto\nWeekDay = ordinal\nHour = discreto\nFood = discreto\nFresh = discreto\nDrinks = discreto\nHome = discreto\nBeauty = discreto\nHealth = discreto\nBaby = discreto\nPets = discreto\n")
+# %%
+# PARTE 3
+# %%
+# PARTE 4
+print("----------------Medias-----------------")
+print(df.mean())
+print("\n----------------Medianas-----------------")
+print(df.median())
+print("\n----------------Moda-----------------")
+print(df.mode())
+# print(df.describe())
